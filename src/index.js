c22963729098f039cd75eff6ff5aca450a3865c0
Yevhenii <yevhenzaviriuha@gmail.com>
2022-04-16 08:38:10 +0300

    changes №1

--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import store from './redux/state';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// import { createRoot } from 'react-dom/client';
+
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// function AppWithCallbackAfterRender() {
+// 	return <App prop={state} addPost={addPost} />
+// }
+// root.render(<AppWithCallbackAfterRender />);
+
+let renderEntireTree = (state) => {
+	ReactDOM.render(
+		<React.StrictMode>
+			<BrowserRouter>
+				<App state={state} dispatch={store.dispatch.bind(store)} />
+			</BrowserRouter>
+		</React.StrictMode>,
+		document.getElementById('root')
+	);
+}
+
+renderEntireTree(store.getState());
+
+store.subscribe(renderEntireTree);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
